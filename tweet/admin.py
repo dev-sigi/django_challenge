b3b6e13c99550611f0bf12ce8bcff4b8f49f5a09
dev-sigi <kangsik@korea.ac.kr>
2024-11-05 13:13:58 +0900

    feat: tweet admin 개선
    - Search by payload
    - Search by username of user foreign key.
    - Filter by created_at
    - Make a CUSTOM filter for Tweets that contain and don't contain the words Elon Musk

--- tweet/admin.py
+++ tweet/admin.py
@@ -2,11 +2,35 @@ from django.contrib import admin
 from .models import Tweet, Like
 
 
+class ElonMuskFilter(admin.SimpleListFilter):
+
+    title = "ElonMuskFilter"
+    parameter_name = "elon_musk_filter"
+
+    def lookups(self, request, model_admin):
+        return (("Elon Musk", "Elon Musk"), ("Not Elon Musk", "Not Elon Musk"))
+
+    def queryset(self, request, tweet):
+        if self.value() == "Elon Musk":
+            return tweet.filter(payload__contains="Elon Musk")
+        if self.value() == "Not Elon Musk":
+            return tweet.exclude(payload__contains="Elon Musk")
+
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
-    list_display = ("user", "user__name","payload", "created_at", "updated_at", "total_likes",)
-    search_fields = ("user__name",)
-    list_filter = ("created_at", "updated_at",)
+    list_display = ("user",
+                    "user__name",
+                    "payload",
+                    "created_at",
+                    "updated_at",
+                    "total_likes",)
+    search_fields = ("user__name",
+                     "payload", # Search by payload.
+                    )
+    list_filter = ("created_at", # Filter by create_at.
+                   "updated_at",
+                   ElonMuskFilter,
+                  )
 
     def total_likes(self, tweet: Tweet):
         return tweet.likes.count()
