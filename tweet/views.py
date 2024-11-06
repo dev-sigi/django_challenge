7c48dc09a238c80f4b7f64d4ffb348118b1c7f28
dev-sigi <kangsik@korea.ac.kr>
2024-11-06 13:10:28 +0900

    FEAT: show all tweets
    [수정]
    1. config/urls.py
      - tweet url 추가
    2. tweet/views.py
      - all_tweet 생성
      - Django orm을 사용해서 모든 트윗을 가져옴
    
    [생성]
    1. tweet/templates/all_tweet.html
      - 모든 트윗을 보여주는 페이지
    2. tweet/urls.py
      - all_tweet url 연결

--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tweet
 
-# Create your views here.
+def all_tweet(request):
+  tweets = Tweet.objects.all()
+  return render(request, "all_tweet.html", {"tweets": tweets, "title": "All Tweets",},)
