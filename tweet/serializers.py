6bdeab54c511efa8b2781292096206b54fcbc839
dev-sigi <kangsik@korea.ac.kr>
2024-11-08 09:32:57 +0900

    STYLE: 과거 코드 주석 제거
    - tweet/serializers.py
    - tweet/views.py

--- tweet/serializers.py
+++ tweet/serializers.py
@@ -1,19 +1,6 @@
 from rest_framework import serializers
 from .models import Tweet
 
-
-# class TweetListSerializer(serializers.Serializer):
-
-#     pk = serializers.IntegerField(read_only=True)
-#     payload = serializers.CharField(required=True, max_length=180)
-#     user = serializers.SerializerMethodField()
-#     created_at = serializers.DateTimeField(read_only=True)
-#     updated_at = serializers.DateTimeField(read_only=True)
-
-#     def get_user(self, tweet):
-#         return {"user_id": tweet.user.user_id, "name": tweet.user.name}
-
-
 class TweetListSerializer(serializers.ModelSerializer):
 
     user = serializers.SerializerMethodField()
--- tweet/views.py
+++ tweet/views.py
@@ -5,19 +5,6 @@ from django.shortcuts import render
 from tweet.serializers import TweetListSerializer
 from .models import Tweet
 
-
-# @api_view(["GET"])
-# def all_tweet(request):
-#   tweets = Tweet.objects.all()
-#   serializer = TweetListSerializer(tweets, many=True)
-#   return render(request,
-#                 "all_tweet.html",
-#                 {
-#                   "tweets": serializer.data,
-#                   "title": "All Tweets",
-#                 },
-#               )
-
 class TweetList(APIView):
 
   def get(self, request):
